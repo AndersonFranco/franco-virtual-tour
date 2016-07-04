24f2fcd8b069c21f89d08275421c0d3e5a677ab0
Anderson Franco <anderson@francotecnologia.com>
2016-07-03 22:27:37 -0300

    1.0.0

---
+++ streetview-virtualtour.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+<?php if (is_single()): ?>
+<div class="franco_virtualtour-single">
+	<iframe src="<?php echo esc_html(get_post_meta(get_the_ID(), 'streetview_url', true)); ?>" height="600" frameborder="0" width="100%"></iframe>
+	<div class="info">
+		<h3><span class="title"><?php the_title(); ?></span><span class="content"><?php echo trim($post->post_content) != '' ? ' - ' . $post->post_content : ''; ?></span><a href="<?php echo esc_html(get_post_meta(get_the_ID(), 'streetview_url', true)); ?>" class="btn right" target="_blank">Open in Google Maps</a></h3>
+	</div>
+</div>
+<?php endif; ?>
+<div id="primary" class="franco_virtualtour-index">
+    <div id="content" role="main">
+    <h2>Google Street View</h2>
+    <?php $loop = new WP_Query(array('post_type' => 'franco_virtualtour')); ?>
+    <?php $i = 0; while ($loop->have_posts()): $loop->the_post(); ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header>
+                <a href="<?php echo get_permalink(); ?>">
+                	<?php the_post_thumbnail(array(320, 200)); ?><br>
+                	<span class="title"><?php the_title(); ?></span><br>
+                    <span class="content"><?php the_category(', '); ?></span>
+                </a>
+            </header>
+        </article>
+        <?php if (++$i % 3 == 0): ?><div style="height:0">&nbsp;</div><?php endif; ?>
+    <?php endwhile; ?>
+    </div>
+</div>
+<?php wp_reset_query(); ?>
+<?php get_footer(); ?>
---
+++ virtualtour.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Plugin Name: Franco Virtual Tour
+ * Plugin URI: https://github.com/AndersonFranco/virtual-tour-gallery
+ * Description: Street View Gallery.
+ * Author: Anderson Franco
+ * Author URI: http://www.francotecnologia.com/
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ */
+class FrancoVirtualTour
+{
+
+    public function __construct()
+    {
+        register_activation_hook(__FILE__, array(get_called_class(), 'pluginActivation'));
+        add_action('plugins_loaded', array(get_called_class(), 'actions'));
+    }
+
+    public function pluginActivation()
+    {
+        self::createPostType();
+        flush_rewrite_rules();
+    }
+
+    public function actions()
+    {
+        add_action('init', array(get_called_class(), 'createPostType'));
+        add_action('add_meta_boxes', array(get_called_class(), 'addMetaBoxes'));
+        add_action('save_post', array(get_called_class(), 'savePost'));
+        add_action('after_switch_theme', array(get_called_class(), 'pluginActivation'));
+        add_action('wp_enqueue_scripts', array(get_called_class(), 'css'), 98);
+        add_filter('template_include', array(get_called_class(), 'template'), 1);
+        add_filter('pre_get_document_title', array(get_called_class(), 'pageTitle'), 100, 1);
+    }
+
+    public function createPostType()
+    {
+        register_post_type('franco_virtualtour',
+            array(
+                'labels' => array(
+                    'name' => __('Virtual Tours'),
+                    'singular_name' => __('Virtual Tour')
+                ),
+                'taxonomies' => array('category'),
+                'public' => true,
+                'supports' => array('title', 'editor', 'thumbnail'),
+                'has_archive' => true,
+                'rewrite' => array('slug' => 'virtualtour'),
+            )
+        );
+    }
+
+    public function addMetaBoxes()
+    {
+        add_meta_box('meta_box', __('Extras'), array(get_called_class(), 'metaBoxContent'), 'franco_virtualtour', 'normal', 'high');
+    }
+
+    public function metaBoxContent($post)
+    {
+        $streetviewUrl = esc_html(get_post_meta($post->ID, 'streetview_url', true));
+        wp_nonce_field(plugin_basename(__FILE__), 'box_content_nonce');
+        echo '<label for="streetview_url">Street View Embed URL</label>';
+        echo '<input type="text" id="streetview_url" name="streetview_url" placeholder="Street View URL" value="' . $streetviewUrl . '" style="width:100%;" />';
+    }
+
+    public function savePost($postId)
+    {
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+        if (!wp_verify_nonce($_POST['box_content_nonce'], plugin_basename(__FILE__))) return;
+
+        if ('franco_virtualtour' == $_POST['post_type']) {
+            update_post_meta($postId, 'streetview_url', $_POST['streetview_url']);
+        }
+    }
+
+    public function css()
+    {
+        wp_enqueue_style('virtualtour', plugins_url('virtualtour.css', __FILE__), array(), '1.0', 'all');
+    }
+
+    public function template($templatePath)
+    {
+        if (get_post_type() == 'franco_virtualtour') {
+            if ($themeFile = locate_template(array('streetview-virtualtour.php'))) {
+                $templatePath = $themeFile;
+            } else {
+                $templatePath = plugin_dir_path(__FILE__) . '/streetview-virtualtour.php';
+            }
+        }
+        return $templatePath;
+    }
+
+    public function pageTitle($title)
+    {
+        return is_single() ? $title : 'Google Street View - Virtual Tours';
+    }
+
+}
+
+new FrancoVirtualTour();
